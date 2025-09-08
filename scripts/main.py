5beda8bc8725aef7074070c03ca66ce0c4932edc
krutkev <i.krutkevych@ukma.edu.ua>
2025-09-08 23:10:25 +0300

    Add base script files

---
+++ scripts/main.py
@@ -0,0 +1,7 @@
+import bot_core
+
+
+if __name__ == "__main__":
+    print("Bot started")
+
+    bot_core.bot.polling()
---
+++ scripts/scheduler.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+
+df = pd.read_excel("./schedules/CS_schedule.xlsx")
+
+
+print(df)
