20ed55cffb5b2d071fa000c4b556e96e652f39ad
Mafusail2020 <krutkev@gmail.com>
2025-09-10 11:31:48 +0300

    Update schedule code

--- scripts/bot_core.py
+++ scripts/bot_core.py
@@ -2,4 +2,8 @@ import telebot
 
 
 TOKEN = ""
+with open("TOKEN.txt", "r") as file:
+    TOKEN = file.read()
 bot = telebot.TeleBot(token=TOKEN)
+
+
--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -1,7 +1,20 @@
 import pandas as pd
 
 
+class Lesson:
+    def __init__(self, name, time, place, teacher, group):
+        self.name = name
+        self.time = time
+        self.place = place
+        self.teacher = teacher
+        self.group = group
+
+
 df = pd.read_excel("./schedules/CS_schedule.xlsx")
+week = {}
 
-
-print(df)
+for i in range(len(df)):
+    if not pd.isna(df.iloc[i, 0]):
+        if df.iloc[i, 0] in ["Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота"]:
+            week[df.iloc[i, 0]] = i
+print(week)
